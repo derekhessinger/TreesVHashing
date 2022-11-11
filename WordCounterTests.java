ebf1841e05c484223978162f39cb3e31322109a0
Derek Hessinger <derekjhessinger@gmail.com>
2022-11-11 15:22:09 -0500

    init

---
+++ WordCounterTests.java
@@ -0,0 +1,22 @@
+/*
+* File: WordCounterTests.java
+* Derek Hessinger
+* CS231 B
+* 11/11/2022
+*/
+
+public class WordCounterTests{
+
+	public static void main(String[] args){
+
+		// case 1: testing WordCounter("bst")
+		{
+
+			// setup
+			WordCounter wc = new WordCouner("bst");
+
+			// verify
+			System.out.println()
+		}
+	}
+}
